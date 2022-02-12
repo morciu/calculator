d4ed608cf645b58ba681aa8327008be834dbfc5f
morciu <marianpralea@gmail.com>
2022-02-12 12:17:34 +0200

    Basic buggy calculations, operators repeat for now and user must use equal sign.

--- script.js
+++ script.js
@@ -4,12 +4,39 @@ const screen = document.querySelector("#screen");
 // Get all number buttons
 const nr_buttons = document.querySelectorAll(".nr");
 
-// Add click event to all number buttons
+// Get all operators
+const operators = document.querySelectorAll(".operator");
+
+// Equals Button
+const equals = document.querySelector('#equals');
+
+// Get Clear button
+const clearBtn = document.querySelector('#clearBtn');
+
+// Flag to stop adding an operator after another operator
+let operatorBlock = false;
+
+// Variables to store calculations
+let a = 0;
+let b = 0;
+let currentOperator;
+
+// Number clicks
 nr_buttons.forEach(button => {
     button.addEventListener('click', function() {
         printOnDisplay(button);
     });
 });
+
+// Operator clicks
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        addOperator(operator.dataset.value);
+    });
+});
+
+// Clear click
+clearBtn.addEventListener('click', () => {clear()});
 
 // Function to print nr on calculator display
 function printOnDisplay(btn) {
@@ -19,4 +46,54 @@ function printOnDisplay(btn) {
     else {
         screen.innerText += btn.dataset.value;
     }
+    operatorBlock = false;
+}
+
+// Clear all entries
+function clear() {
+    a = 0;
+    b = 0;
+    screen.innerText = '0';
+}
+
+// Equals click
+equals.addEventListener('click', () => {
+    a, b = getNumbers();
+    screen.innerText = calculate(currentOperator);
+})
+
+
+function addOperator(operator) {
+    if (screen.innerText != '0' && (!operatorBlock)) {
+        screen.innerText += ` ${operator}`;
+        operatorBlock = true;
+        a, b = getNumbers();
+    }
+
+    currentOperator = operator;
+}
+
+function calculate(operator) {
+    if (operator == '+') {
+        return a + b;
+    }
+    else if (operator == '-') {
+        return a - b;
+    }
+    else if (operator == '*') {
+        return a * b;
+    }
+    else if (operator == '/') {
+        return a / b;
+    }
+}
+
+function getNumbers() {
+    a = parseInt(screen.innerText.split(` ${currentOperator}`)[0]);
+    b = parseInt(screen.innerText.split(` ${currentOperator}`)[1]);
+
+    a = (Number.isNaN(a)) ? 0 : a;
+    b = (Number.isNaN(b)) ? 0 : b;
+
+    return a, b;
 }
